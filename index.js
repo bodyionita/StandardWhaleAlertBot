2dc21a16fa25d121ae32169be6aa6096ff036d8a
Bogdan Ionita <bogdan.ionita96@gmail.com>
2021-12-14 14:44:55 +0000

    Grab data tested

--- index.js
+++ index.js
@@ -1,2 +1,38 @@
 const keep_alive = require('./keep_alive')
 keep_alive.keep_alive()
+
+const Realm = require('realm-web');
+const app = new Realm.App({ id: 'stakeborgdaoexplorer-wutjr' })
+
+user = null;
+
+async function get_data() {
+  user = await app.logIn(Realm.Credentials.anonymous());
+  client = app.currentUser.mongoClient("mongodb-atlas");
+  dbdata = await client
+        .db("stakeborgdao-explorer")
+        .collection("snapshot")
+        .find({}, { sort: { snapshot: -1 }, limit: 1 });
+  return dbdata;
+}
+
+
+
+
+async function main() {
+  data = await get_data()
+  return data;
+}
+
+main()
+    .then(text => {
+        console.log(text);
+    })
+    .catch(err => {
+        // Deal with the fact the chain failed
+    });
+
+
+
+
+    
